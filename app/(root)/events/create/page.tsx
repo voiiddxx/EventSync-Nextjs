e2596377560829fbc93aa1700e2cfcc958578b77
voiiddxx <95859137+voiiddxx@users.noreply.github.com>
2024-01-16 15:25:46 +0530

    some form implementation

---
+++ app/(root)/events/create/page.tsx
@@ -0,0 +1,13 @@
+
+const page = () => {
+  return (
+    <div className="min-h-screen w-full bg-slate-50">
+     <div className="h-20 w-full bg-slate-200 flex items-start justify-center flex-col pl-14">
+        <h1>Create Event</h1>
+        <p>Please fill all the imformation details to crerate Event</p>
+     </div>
+    </div>
+  )
+}
+
+export default page
--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
     publicRoutes:[
       "/",
-      "events/create",
+      "/events/create",
       "/api/webhook/clerk"
     ],
     ignoredRoutes:[
